bcce83c72e7ad319963f8f30393f4968f9244db0
Maidy Cabrera <maidyc914@gmail.com>
2023-03-26 17:33:26 -0500

    gh-pages commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
-// module.exports = {
-//   publicPath: process.env.NODE_ENV === "production" ? "/Algoritmos_Atenea/" : "/",
-// };
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/Algoritmos_Atenea_Page_Vue/" : "/",
+};
